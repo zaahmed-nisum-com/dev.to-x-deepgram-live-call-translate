b6c74396f3487958a78b46ae02b82ba5bdf086e2
zaahmed-nisum-com <zain.ahmed199524@gmail.com>
2022-03-24 12:40:08 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Streaming from './pages/streaming';
+
+const APP_ID = '20a9600e27274878acd571fbb8ca7f0f'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Streaming APP_ID={APP_ID} />
   );
 }
 
